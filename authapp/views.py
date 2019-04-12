fd14b9c9a58630d41d3b4ab82f1872ee26f0c12b
gobzter@gmail.com <123321Qq!>
2019-04-12 23:11:56 +0300

    Починил requirements.txt, вырезал GroupSerializer

--- authapp/views.py
+++ authapp/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import Group
 from django.core.mail import send_mail
 from django.db import transaction
 from django.shortcuts import render, HttpResponseRedirect
@@ -10,7 +9,7 @@ from django.urls import reverse
 from django.conf import settings
 
 from authapp.models import GiftUser
-from authapp.serializers import GiftUserSerializer, GiftGroupSerializer
+from authapp.serializers import GiftUserSerializer
 
 
 def send_verify_mail(user):
