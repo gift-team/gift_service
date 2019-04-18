86c11dc29ce54423db32cb5d723adfb4bd6c5cf5
gobzter@gmail.com <123321Qq!>
2019-04-18 13:17:22 +0300

    Установил REST swagger

--- gift_service/urls.py
+++ gift_service/urls.py
@@ -17,12 +17,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from gift_service import settings
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Pastebin API')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authapp.urls', namespace='auth')),
-    path('api-auth/', include('rest_framework.urls'), name='rest_framework')
+    path('api-auth/', include('rest_framework.urls'), name='rest_framework'),
+    path('swagger/', schema_view)
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
