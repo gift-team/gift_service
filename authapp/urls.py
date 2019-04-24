30a88cc5f615c9abeb9d8a7371bdb6c1b6e213ba
gobzter@gmail.com <123321Qq!>
2019-04-24 15:11:31 +0300

    Исправил УРЛы

--- authapp/urls.py
+++ authapp/urls.py
@@ -15,7 +15,7 @@ app_name = 'authapp'
 urlpatterns = [
     # path('login/', authapp.login, name='login'),
     path('logout/', authapp.logout, name='logout'),
-    path('register/', authapp.register, name='register'),
+    # path('register/', authapp.register, name='register'),
     path('edit/', authapp.edit, name='edit'),
     path('verify/<str:email>/<str:activation_key>', authapp.verify, name='verify'),
 
