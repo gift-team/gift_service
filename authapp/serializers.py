139f11c5a64df0a07dfceab38a25781c67b48e30
sensactive <sensor14@list.ru>
2019-04-25 09:20:31 +0300

    изменил create в сериализаторе

--- authapp/serializers.py
+++ authapp/serializers.py
@@ -15,7 +15,7 @@ class GiftUserSerializer(serializers.HyperlinkedModelSerializer):
         password = validated_data.pop('password', None)
         instance = self.Meta.model(**validated_data)
         if password is not None:
-            instance.set_password(make_password(password))
+            instance.set_password(password)
         instance.save()
         return instance
 
