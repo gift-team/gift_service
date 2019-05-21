48dedf2208b0e658790f6166fee0da220b96a322
gobzter@gmail.com <123321Qq!>
2019-05-21 10:33:18 +0300

    Поправил выдачу first_name или email'а в зависимости от наличия first_name у пользователя. Добавил PUT и PATCH запросы в редактирование профиля

--- gift_service/settings.py
+++ gift_service/settings.py
@@ -161,6 +161,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+        'authapp.permissions.IsOwnerOnly',
     )
 }
 
